335685013d22c3e229daaaca62776b1cf56b8842
Wizzzzzzard <56865341+Wizzzzzzard@users.noreply.github.com>
2021-05-10 17:01:09 +0100

    Sqrt model, all variables included

---
+++ 2. SQRT No Multicolinearity or Values Removed Prediction BAE.R
@@ -0,0 +1,16 @@
+predict_BAE <- function(lse, newdata){
+  # Carry out any transformations prior to fitting you model
+  # Add transformed variables to both lse and newdata. E.g.:
+  lse$BA.log <- log(lse$BA)
+  newdata$BA.log <- log(newdata$BA)
+  
+  # this is the part that fits your linear model
+  BAE.lm <- lm(sqrt(BA) ~ STJ + DLG + BATS + VOD + RTO + GVC + CPG + SPX + SSE + PRU
+               + AHT + RB + MIN + RMV + Year + EXPN + TSCO + SDR + CCH + NMC
+               + SMT + TUI + LLOY + CNA + Month, data = lse)
+  
+  # this is the part that produces predictions using your linear model
+  predictions <- predict(BAE.lm, newdata = newdata)
+  predictions <- predictions**2
+  return(predictions)
+}
